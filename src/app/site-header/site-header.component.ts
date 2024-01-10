5abfd8d9da014439b986fc9f64ed5b844c74e027
mwirzeski <mwirzeski@future-processing.com>
2024-01-10 14:58:10 +0100

    components

---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-site-header',
+  standalone: true,
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css']
+})
+export class SiteHeaderComponent {
+
+  constructor() { }
+
+}
